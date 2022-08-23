74b57365b8d34583631c95e293245cf5fec87d5b
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-23 17:18:07 +0100

    Create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { Container, AppBar, Typography } from '@mui/material';
+import memories from './images/memories.png';
+import {useStyles} from './styles';
+
 function App() {
-  return (
+  const classes = useStyles;
+return (
     <>
-      
+      <Container maxwidth="lg">
+        <AppBar position="static" color="inherit" style={classes.appBar}>
+          <Typography style={classes.heading} variant="h2" align="center">Memories</Typography>
+          <img src={memories} alt="memories" height="60" width="60" style={classes.image}/>
+        </AppBar>
+      </Container>
     </>
   );
 }
