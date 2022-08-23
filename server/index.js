735cb0d39875da96288eea69fb2d53a1b2be922f
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-23 09:52:24 +0100

    prepare backend and Database connection

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit : "30mb" , extended : true}));
+app.use(bodyParser.urlencoded({ limit : "30mb" , extended : true}));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000 ;
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true , useUnifiedTopology : true})
+.then(() => {
+    app.listen(PORT, () => {
+    console.log("Server is running on port "+PORT);
+    }
+    )
+}).catch(err => {
+    console.log("Error: ", err.message);
+}
+);
