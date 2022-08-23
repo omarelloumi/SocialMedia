cca4dcfb707202226c5e7d201482b43d8df45997
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-23 10:03:39 +0100

    Create Post model

---
+++ server/models/postMessage.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    selectedFile: String,
+    likeCount: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const postMessage = mongoose.model("PostMessage", postSchema);
+
+export default postMessage;
