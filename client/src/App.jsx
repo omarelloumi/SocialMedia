59a21ce24b1f899b39820885a802dcfcfdad4197
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-29 01:39:08 +0100

    prepare router for authentification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import { Container } from '@mui/material';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Auth from './components/Auth/Auth';
 function App() {
   
   return (
     <>
-      <Container maxwidth="lg">
-        <Navbar/>
-        <Home/>
-      </Container>
+      <Router>
+        <Container maxWidth="lg">
+          <Navbar />
+          <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          </Routes>
+        </Container>
+      </Router>
     </>
   );
 }
---
+++ client/src/components/Auth/Auth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>This is auth</div>
+  )
+}
+
+export default Auth
