a2446ddc8ad3b4bfe52930f7bfe0f26809d875ba
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-31 04:27:33 +0100

    use routes in backend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const postRoutes = require('./routes/postRoute');
+const userRoutes = require('./routes/userRoute');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ limit : "30mb" , extended : true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000 ;
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true , useUnifiedTopology : true})
