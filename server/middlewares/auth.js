8013e0bd9f98ee5b046ae558e5d3313932bd1b28
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-08-31 18:23:43 +0100

    use middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -25,4 +25,4 @@ const auth = async (req, res, next) => {
   }
 };
 
-export default auth;
+module.exports = auth;
--- server/routes/postRoute.js
+++ server/routes/postRoute.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const { getPosts, createPost, updatePost, deletePost, likePost } = require('../controllers/postController');
+const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.get('/',getPosts);
 
-router.post('/',createPost);
+router.post('/',auth,createPost);
 
-router.patch('/:id',updatePost);
+router.patch('/:id',auth,updatePost);
 
-router.delete('/:id',deletePost);
+router.delete('/:id',auth,deletePost);
 
-router.patch('/:id/likePost',likePost);
+router.patch('/:id/likePost',auth,likePost);
 
 module.exports = router;
