dd7c705e88b648ffdcad1b1b2bb066d14644cfad
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-02 13:40:23 +0100

    Prepare routing
    routing for search , post details and authentication redirection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import { Container } from '@mui/material';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PostDetails from "./components/PostDetails/PostDetails";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Auth from './components/Auth/Auth';
 function App() {
-  
+  const user = JSON.parse(localStorage.getItem('profile'));
+  const navigate = useNavigate();
   return (
     <>
       <Router>
-        <Container maxWidth="lg">
+        <Container maxWidth="xl">
           <Navbar />
           <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route path='/' element={() => navigate('/posts')} />
+          <Route path='/posts' element={<Home />} />
+          <Route path='/posts/search' element={<Home />} />
+          <Route path='/posts/:id' element={<PostDetails/>} />
+          <Route path="/auth" element={() => (!user ? <Auth /> : navigate('/posts'))} />
           </Routes>
         </Container>
       </Router>
