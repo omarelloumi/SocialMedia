8b54a24261f4fb53601ae9f0cab52c314e8b43fb
omarelloumi <48672001+omarelloumi@users.noreply.github.com>
2022-09-06 05:37:56 +0100

    add styles for post details

---
+++ client/src/components/PostDetails/styles.js
@@ -0,0 +1,35 @@
+const useStyles = {
+  media: {
+    borderRadius: "20px",
+    objectFit: "cover",
+    width: "100%",
+    maxHeight: "600px",
+  },
+  card: {
+    display: "flex",
+    width: "100%",
+    flexWrap: "wrap",
+    flexDirection: "column",
+  },
+  section: {
+    borderRadius: "20px",
+    margin: "10px",
+    flex: 1,
+  },
+  imageSection: {
+    marginLeft: "20px",
+  },
+  recommendedPosts: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  loadingPaper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "20px",
+    borderRadius: "15px",
+    height: "39vh",
+  },
+};
+export default useStyles;
